b852bed1724c773c85fe83518543749002da4d6e
RafalSwat <rafal.swat@gmail.com>
2021-05-11 10:49:29 +0200

    Initial Commit

---
+++ PlayingWithGestures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayingWithGestures
+//
+//  Created by Rafał Swat on 11/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
